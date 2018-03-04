934423f3642b3225a8b955245b1471c882bebec8
Mohan15 <pmohanraj108@gmail.com>
2018-03-04 19:58:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@ var img=document.getElementById('madi');
 
 var marginLeft=0;
 function moveright(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
 img.style.marginLeft=marginLeft+'px';
 
 }
 
 img.onclick = function(){
-    var interval=setInterval(moveright,100);
+    var interval=setInterval(moveright,10);
 };
