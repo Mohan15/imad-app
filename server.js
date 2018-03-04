ffb7aebcc09fbb2750a34484dd097e3c0e3a71a2
Mohan15 <pmohanraj108@gmail.com>
2018-03-04 16:37:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,72 +4,84 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles={
- 'article-one':{
-     title:'Article-one| Mohanraj',
-     heading:'Article-one',
-     date:'Aug - 23 - 2017',
-     content:`<p>This is my first webpage and that too tanmai is teaching good also i got many points through him.
-            This is my first webpage and that too tanmai is teaching good also i got many points through him.
-            This is my first webpage and that too tanmai is teaching good also i got many points through him.</p>
+// var articles={
+//  'article-one':{
+//      title:'Article-one| Mohanraj',
+//      heading:'Article-one',
+//      date:'Aug - 23 - 2017',
+//      content:`<p>This is my first webpage and that too tanmai is teaching good also i got many points through him.
+//             This is my first webpage and that too tanmai is teaching good also i got many points through him.
+//             This is my first webpage and that too tanmai is teaching good also i got many points through him.</p>
             
-             <p>This is my first webpage and that too tanmai is teaching good also i got many points through him.
-            This is my first webpage and that too tanmai is teaching good also i got many points through him.
-            This is my first webpage and that too tanmai is teaching good also i got many points through him.</p>`
+//              <p>This is my first webpage and that too tanmai is teaching good also i got many points through him.
+//             This is my first webpage and that too tanmai is teaching good also i got many points through him.
+//             This is my first webpage and that too tanmai is teaching good also i got many points through him.</p>`
         
-},
-'article-two':{
-     title:'Article-Two| Mohanraj',
-     heading:'Article-Two',
-     date:'Aug - 25 - 2017',
-     content:`<p>This is my Third webpage and that too tanmai is teaching good also i got many points through him.
-           </p>`
- },
-'article-three':{
-      title:'Article-Three| Mohanraj',
-     heading:'Article-Three',
-     date:'Aug - 29 - 2017',
-     content:`<p>This is my Third webpage and that too tanmai is teaching good also i got many points through him.
-           </p>`
- }
-};
+// },
+// 'article-two':{
+//      title:'Article-Two| Mohanraj',
+//      heading:'Article-Two',
+//      date:'Aug - 25 - 2017',
+//      content:`<p>This is my Third webpage and that too tanmai is teaching good also i got many points through him.
+//           </p>`
+//  },
+// 'article-three':{
+//       title:'Article-Three| Mohanraj',
+//      heading:'Article-Three',
+//      date:'Aug - 29 - 2017',
+//      content:`<p>This is my Third webpage and that too tanmai is teaching good also i got many points through him.
+//           </p>`
+//  }
+// };
 
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    var htmlTemplate=`
-                    <html>
-                        <head>
-                             <title> ${title}</title>
-                             <meta name="viewport" content="width=device-width, initial-scale=1" />
-                             <link href="/ui/style.css" rel="stylesheet" />
-                        </head>
-                    <body>
+// function createTemplate(data){
+//     var title=data.title;
+//     var heading=data.heading;
+//     var date=data.date;
+//     var content=data.content;
+//     var htmlTemplate=`
+//                     <html>
+//                         <head>
+//                              <title> ${title}</title>
+//                              <meta name="viewport" content="width=device-width, initial-scale=1" />
+//                              <link href="/ui/style.css" rel="stylesheet" />
+//                         </head>
+//                     <body>
                         
-                        <div class='container'>
-                            <div>
-                                <a href='/'>Home</a>
-                            </div>
-                            <hr/>
-                            <h3> ${heading}</h3>
-                            <div>
-                                 ${date}
-                            </div>
-                            <div>
-                               ${content}
-                            </div>
+//                         <div class='container'>
+//                             <div>
+//                                 <a href='/'>Home</a>
+//                             </div>
+//                             <hr/>
+//                             <h3> ${heading}</h3>
+//                             <div>
+//                                  ${date}
+//                             </div>
+//                             <div>
+//                               ${content}
+//                             </div>
                            
-                        </div>
+//                         </div>
                         
-                    </body>
-                    </html> `;
-    return htmlTemplate;
-}
+//                     </body>
+//                     </html> ;
+//     return htmlTemplate;
+// }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
