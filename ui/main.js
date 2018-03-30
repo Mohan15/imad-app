569198c3a6635852941123f92cd4d3a43727eb36
Mohan15 <pmohanraj108@gmail.com>
2018-03-30 19:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,19 @@
-/*console.log('Loaded!');
-
-//To Change the elemnt as "My New Page"
-var element=document.getElementById('mainpage');
-element.innerHTML="My New Page";
-
-//To Move the Image
-var img=document.getElementById('madi');
-
-var marginLeft=0;
-function moveright(){
-    marginLeft=marginLeft+1;
-img.style.marginLeft=marginLeft + 'px';
-
-}
-
-img.onclick = function(){
-    var interval=setInterval(moveright,50);
-};*/
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onClick = function () {
     //request to counter endpoint
+    var request=new XMLHttpRequest();
     
     //get the response and store it in a varible 
-    
-    //render the varible with span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange =function(){
+        if(request.readystate===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
     
 };
