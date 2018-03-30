df43820b448b4c0ab7577a2e9afd02bf4a8d5187
Mohan15 <pmohanraj108@gmail.com>
2018-03-30 19:55:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 console.log('Loaded!');
 
 //To Change the elemnt as "My New Page"
-var element=document.getElementById('mainpage');
-element.innerHTML="My New Page";
+
 
 //To Move the Image
 var img=document.getElementById('madi');
