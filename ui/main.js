f7664657e47d773eeb1d1fb67586cb0e925c6855
Mohan15 <pmohanraj108@gmail.com>
2018-03-30 18:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,15 +19,15 @@ img.onclick = function(){
 };*/
 
 var button = document.getElementById('counter');
-var counter=0;
-button.onClick= function(){
+var counter = 0;
+button.onClick = function(){
     //request to counter endpoint
     
     //get the response and store it in a varible 
     
     //render the varible with span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
     
 };
